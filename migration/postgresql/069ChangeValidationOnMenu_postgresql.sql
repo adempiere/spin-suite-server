a273affe14cd6a80eb438b2ba384a1bb4ae00024
Yamel Senih <Yamel Senih@localhost>
2014-04-22 22:26:23 +0200

    Change Callout

---
+++ migration/postgresql/069ChangeValidationOnMenu_postgresql.sql
@@ -0,0 +1,10 @@
+-- Apr 22, 2014 10:24:22 PM CEST
+-- Spin-Suite
+UPDATE AD_Tab SET WhereClause='SPS_Menu.MenuType = ''A''',Updated=TO_TIMESTAMP('2014-04-22 22:24:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53917
+;
+
+-- Apr 22, 2014 10:24:38 PM CEST
+-- Spin-Suite
+UPDATE AD_Tab SET WhereClause='SPS_Menu.MenuType = ''M''',Updated=TO_TIMESTAMP('2014-04-22 22:24:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53677
+;
+
