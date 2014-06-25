c21d1989476cf16e2105a9f8bb9868325927870e
Dixon Martinez <Dixon Martinez@localhost>
2014-06-25 10:08:52 -0530

    Update columns

--- migration/postgresql/801AddAlterTables.sql
+++ migration/postgresql/801AddAlterTables.sql
@@ -68,3 +68,79 @@ UPDATE AD_Form SET IsDefinedForMobile = 'N' WHERE IsDefinedForMobile IS NULL
 -- SFAndroid Server
 ALTER TABLE SPS_Column ADD COLUMN AD_Process_ID NUMERIC(10) DEFAULT NULL 
 ;
+
+-- Jun 25, 2014 10:53:46 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Menu ADD COLUMN MenuType CHAR(1) NOT NULL
+;
+
+-- Jun 25, 2014 10:54:27 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Menu ADD COLUMN ActivityMenu_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 10:56:50 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Tab ADD COLUMN SPS_Column_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 10:57:29 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN AD_FieldGroup_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 10:58:09 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Tab ADD COLUMN Parent_Column_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 10:58:29 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Tab ADD COLUMN AD_Process_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:01:20 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Tab ADD COLUMN IsInsertRecord CHAR(1) DEFAULT 'N' CHECK (IsInsertRecord IN ('Y','N'))
+;
+
+-- Jun 25, 2014 11:02:07 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN IsCentrallyMaintained CHAR(1) DEFAULT 'Y' CHECK (IsCentrallyMaintained IN ('Y','N'))
+;
+
+-- Jun 25, 2014 11:02:41 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN DisplayLogic VARCHAR(2000) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:03:02 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN AD_Reference_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:03:20 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN AD_Val_Rule_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:03:42 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN AD_Reference_Value_ID NUMERIC(10) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:04:10 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN IsMandatory CHAR(1) DEFAULT NULL 
+;
+
+-- Jun 25, 2014 11:04:51 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN IsEncrypted CHAR(1) DEFAULT 'N' CHECK (IsEncrypted IN ('Y','N'))
+;
+
+-- Jun 25, 2014 11:05:19 AM VET
+-- Spint Suite Server
+ALTER TABLE SPS_Field ADD COLUMN DefaultValue VARCHAR(2000) DEFAULT NULL 
+;
+
