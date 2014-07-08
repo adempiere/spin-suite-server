f96bdd0eaac2a59e681a8eda27e271f6c75f32a3
Dixon Martinez <Dixon Martinez@localhost>
2014-07-08 15:58:21 -0530

    Update Window Sync Menu

---
+++ migration/oracle/073UpdateWindowSyncMenu.sql
@@ -0,0 +1,25 @@
+-- Jul 8, 2014 8:45:09 AM VET
+-- Spin Suite Server
+UPDATE AD_Column SET IsMandatory='N',Updated=TO_DATE('2014-07-08 08:45:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71529
+;
+
+-- Jul 8, 2014 8:45:14 AM VET
+-- Spin Suite Server
+ALTER TABLE SPS_SyncMenu MODIFY SPS_Table_ID NUMBER(10) DEFAULT NULL 
+;
+
+-- Jul 8, 2014 8:45:14 AM VET
+-- Spin Suite Server
+ALTER TABLE SPS_SyncMenu MODIFY SPS_Table_ID NULL
+;
+
+-- Jul 8, 2014 8:47:11 AM VET
+-- Spin Suite Server
+UPDATE AD_Column SET MandatoryLogic='@WS_WebService_ID@ > 0',Updated=TO_DATE('2014-07-08 08:47:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71529
+;
+
+-- Jul 8, 2014 8:50:12 AM VET
+-- Spin Suite Server
+UPDATE AD_Column SET MandatoryLogic='@WS_WebServiceType_ID@ > 0',Updated=TO_DATE('2014-07-08 08:50:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71529
+;
+
