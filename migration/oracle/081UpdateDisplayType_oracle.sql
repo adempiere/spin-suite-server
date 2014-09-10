637cf34b48252b619cf018af12bed94ed8ae6fca
Yamel Senih <Yamel Senih@localhost>
2014-09-10 15:28:30 +0200

    Update Column Display Type

---
+++ migration/oracle/081UpdateDisplayType_oracle.sql
@@ -0,0 +1,10 @@
+-- Sep 10, 2014 3:25:52 PM CEST
+-- Spin-Suite
+UPDATE AD_Column SET AD_Reference_ID=17, AD_Reference_Value_ID=319,Updated=TO_DATE('2014-09-10 15:25:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66049
+;
+
+-- Sep 10, 2014 3:26:03 PM CEST
+-- Spin-Suite
+UPDATE AD_Column SET AD_Reference_ID=17, AD_Reference_Value_ID=319,Updated=TO_DATE('2014-09-10 15:26:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=66050
+;
+
