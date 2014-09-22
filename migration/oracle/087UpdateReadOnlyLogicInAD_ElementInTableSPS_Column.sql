0793edc418d405efb814ff73537e52bd33ccacc4
Dixon Martinez <Dixon Martinez@localhost>
2014-09-22 11:54:15 -0530

    Update callout

---
+++ migration/oracle/087UpdateReadOnlyLogicInAD_ElementInTableSPS_Column.sql
@@ -0,0 +1,5 @@
+-- Sep 22, 2014 12:52:22 PM VET
+-- SPS
+UPDATE AD_Column SET ReadOnlyLogic='@AD_Table_ID@ = 0',Updated=TO_DATE('2014-09-22 12:52:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=71458
+;
+
